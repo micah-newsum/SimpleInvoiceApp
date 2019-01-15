f5c1b7821fddcc8204d83f725e0c1ab366d8f388
Micah <micahnew90@yahoo.com>
2019-01-14 22:23:36 -0800

    Add BigDecimal Code

--- src/InvoiceApp.java
+++ src/InvoiceApp.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 import java.text.NumberFormat;
+import java.math.BigDecimal;
+import java.math.RoundingMode;  //enumeration
+
 /**
  * @author mnewsum
  * This is a simple invoice application that exhibits some of the fundamental components 
@@ -9,7 +12,12 @@ import java.text.NumberFormat;
 public class InvoiceApp {
 
 	public static void main(String[] args) {
-		final double SALES_TAX_PCT = .0925;
+		
+		/*
+		 * It's often best to create BigDecimal objects from strings rather than 
+		 * floating point numbers because float/doubles do not always represent decimal numbers exactly.
+		 */
+		final BigDecimal SALES_TAX_PCT = new BigDecimal(".0925");
 				
 		System.out.println("Simple Invoice Application");
 		System.out.println(); // prints a blank line
@@ -20,25 +28,27 @@ public class InvoiceApp {
 		// perform invoice calculations until choice isn't equal to "y" or "Y"
 		String choice = "y";
 		while (choice.equalsIgnoreCase("y")) {
+			
 			//get the invoice subtotal from the user 
 			System.out.print("Enter subtotal:  ");
-			double subtotal = sc.nextDouble();
+			String subtotalString = sc.next();
 			
-			// get the discount percent
-			double discountPercent;
-			if (subtotal >= 200) {
-				discountPercent = .2;
-			} else if (subtotal >= 100) {
-				discountPercent = .1;				
+			// create the BigDecimal objects
+			BigDecimal subtotal = new BigDecimal(subtotalString);
+			BigDecimal discountPercent;
+			if (subtotal.doubleValue() >= 200) {
+				discountPercent = new BigDecimal(".2");
+			} else if (subtotal.doubleValue() >= 100) {
+				discountPercent = new BigDecimal(".1");				
 			} else {
-				discountPercent = 0.0;
+				discountPercent = new BigDecimal("0.0");
 			}
 			
 			// calculate the discount amount and total 
-			double discountAmount = subtotal * discountPercent;
-			double totalBeforeTax = subtotal - discountAmount;
-			double salesTax = totalBeforeTax * SALES_TAX_PCT;
-			double total = totalBeforeTax + salesTax;
+			BigDecimal discountAmount = subtotal.multiply(discountPercent).setScale(2, RoundingMode.HALF_UP);
+			BigDecimal totalBeforeTax = subtotal.subtract(discountAmount);
+			BigDecimal salesTax = SALES_TAX_PCT.multiply(totalBeforeTax).setScale(2, RoundingMode.HALF_UP);
+			BigDecimal total = totalBeforeTax.add(salesTax);
 			
 			// display the results 
 			NumberFormat currency = NumberFormat.getCurrencyInstance();
